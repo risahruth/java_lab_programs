1be9ee9f5d6d40c6c9978b8e1fa8489fa616bf3f
risahruth <187304967+risahruth@users.noreply.github.com>
2025-07-23 23:43:17 +0530

    Create Operations.java

---
+++ Week1/Operations.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class Operations
+{
+public static void main(String[] args)
+{
+double a,b;char c;
+System.out.println("Enter any two integers or double values");
+Scanner ip=new Scanner(System.in);
+a=ip.nextDouble();
+b=ip.nextDouble();
+System.out.println("Enter the kind of operation you want to perform on the two numbers");
+c=ip.next().charAt(0);
+switch(c){
+case '+':{
+System.out.println(a+b);
+break;
+}
+case '-':{
+System.out.println(a-b);
+break;
+}
+case '*':{
+System.out.println(a*b);
+break;
+}
+case '/':{
+System.out.println(a/b);
+break;
+}
+}}}
+
