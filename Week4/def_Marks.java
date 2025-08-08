fd27e87347f5ba6e35805ab2ea0f27fc385462da
Risah Ruth R <187304967+risahruth@users.noreply.github.com>
2025-08-08 22:48:38 +0530

    Create def_Marks.java

---
+++ Week4/def_Marks.java
@@ -0,0 +1,10 @@
+public class Def_Marks {
+    int num;
+    String name;
+    public static void main(String[] args){
+        Def_Marks obj=new Def_Marks();
+        System.out.println("2024503543 Risah Ruth R");
+        System.out.println("When no constructors are defined : The behaviour of default constructor");
+        System.out.println("Default Number is "+obj.num+"\nDefault string is "+obj.name);
+    }
+}
